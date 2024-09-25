360a4745e17a78e69af484f25e04a2759b72cd11
bernardinolab <bernardinoqw@gmail.com>
2024-09-25 22:31:13 +0100

    fixed nextcloud ios app login error

--- Docker Compose/Docker + Traefik + Lets's Encrypt/nextcloud/deploy_nextcloud.sh
+++ Docker Compose/Docker + Traefik + Lets's Encrypt/nextcloud/deploy_nextcloud.sh
@@ -7,6 +7,11 @@
 
 DOMAIN='your_dynu_domain'
 
+# Time Zone code see here: https://www.php.net/manual/en/timezones.php
+TIMEZONE='Europe/Lisbon'
+# Region code see here: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+REGION='PT'
+
 ###############################################################
 #                         SCRIPT CODE
 ###############################################################
@@ -64,29 +69,29 @@ services:
     # ports:
     #  - 443:443
     labels:
-      - "traefik.enable=true"
-      - "traefik.http.routers.nextcloud.entrypoints=http"
-      - "traefik.http.routers.nextcloud.rule=Host(\`nextcloud.\$DOMAIN\`)"
-      - "traefik.http.middlewares.nextcloud-https-redirect.redirectscheme.scheme=https"
-      - "traefik.http.routers.nextcloud.middlewares=nextcloud-https-redirect"
-      - "traefik.http.routers.nextcloud-secure.entrypoints=https"
-      - "traefik.http.routers.nextcloud-secure.rule=Host(\`nextcloud.\$DOMAIN\`)"
-      - "traefik.http.routers.nextcloud-secure.tls=true"
-      - "traefik.http.routers.nextcloud-secure.service=nextcloud"
-      - "traefik.http.services.nextcloud.loadbalancer.server.port=80"
-      - "traefik.docker.network=proxy"
-      - "traefik.http.routers.nextcloud.middlewares=nextcloud_redirectregex"
-      - "traefik.http.middlewares.nextcloud_redirectregex.redirectregex.permanent=true"
-      - "traefik.http.middlewares.nextcloud_redirectregex.redirectregex.regex=https://(.*)/.well-known/(?:card|cal)dav"
-      - "traefik.http.middlewares.nextcloud_redirectregex.redirectregex.replacement=https://\$\${1}/remote.php/dav"
+      - traefik.enable=true
+      - traefik.http.routers.nextcloud_app.rule=Host(\`nextcloud.\${DOMAIN}\`)
+      - traefik.http.routers.nextcloud_app.entrypoints=https
+      - traefik.http.routers.nextcloud_app.tls.certresolver=letsencrypt
+      - traefik.http.routers.nextcloud_app.middlewares=nextcloud,nextcloud_redirect
+      - traefik.http.routers.nextcloud_app.tls=true
+      - traefik.http.middlewares.nextcloud.headers.customFrameOptionsValue=ALLOW-FROM https://\${DOMAIN}
+      - traefik.http.middlewares.nextcloud.headers.contentSecurityPolicy=frame-ancestors 'self' \${DOMAIN} *.\${DOMAIN}
+      - traefik.http.middlewares.nextcloud.headers.stsSeconds=155520011
+      - traefik.http.middlewares.nextcloud.headers.stsIncludeSubdomains=true
+      - traefik.http.middlewares.nextcloud.headers.stsPreload=true
+      - traefik.http.middlewares.nextcloud.headers.customresponseheaders.X-Frame-Options=SAMEORIGIN
+      - traefik.http.middlewares.nextcloud_redirect.redirectregex.permanent=true
+      - traefik.http.middlewares.nextcloud_redirect.redirectregex.regex=https://(.*)/.well-known/(card|cal)dav
+      - traefik.http.middlewares.nextcloud_redirect.redirectregex.replacement=https://\$\${1}/remote.php/dav/
     networks:
       proxy:
 
 networks:
   proxy:
     external: true
-
 EOM
+
 
 #Depoying the nextcloud container
 echo "================================"
@@ -95,6 +100,22 @@ echo "nextcloud.${DOMAIN}"
 echo "================================"
 sudo docker compose down
 sudo docker compose up -d --force-recreate
+
+# fixing db:
+sleep 5
+sudo docker exec -it nextcloud occ db:add-missing-indices
+# Updating config.php Values
+sudo docker exec -it nextcloud occ config:system:set logtimezone --value=${TIMEZONE}
+sudo docker exec -it nextcloud occ config:system:set default_phone_region --value=${REGION}
+sudo docker exec -it nextcloud occ config:system:set overwritehost --value=nextcloud.${DOMAIN}
+sudo docker exec -it nextcloud occ config:system:set overwriteprotocol --value=https
+sudo docker exec -it nextcloud occ config:system:set maintenance_window_start --value=1
+sudo docker exec -it nextcloud occ config:system:set trusted_domains 0 --value=https://nextcloud.${DOMAIN}
+sudo docker exec -it nextcloud occ config:system:set trusted_domains 1 --value=https://${DOMAIN}
+sudo docker exec -it nextcloud occ config:system:set trusted_domains 2 --value=10.50.10.1
+sudo docker exec -it nextcloud occ config:system:set trusted_proxies 0 --value=10.50.10.1
+
+sudo docker compose down
+sudo docker compose up -d --force-recreate
 sudo docker ps -a --no-trunc --filter name=^/nextcloud$
 sudo docker compose logs --follow
-
